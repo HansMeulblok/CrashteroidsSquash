f090661b3ed67ff34f01757489494b22bfab5c36
HansMeulblok <hansmeulblok4@gmail.com>
2021-10-11 11:26:15 +0100

    added laser test

--- Assets/RW/Tests/Tests.cs
+++ Assets/RW/Tests/Tests.cs
@@ -1,28 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using NUnit.Framework;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
 
-namespace Tests
+public class LaserTest
 {
-    public class Tests
-    {
-        // A Test behaves as an ordinary method
-        [Test]
-        public void TestsSimplePasses()
-        {
-            // Use the Assert class to test conditions
-        }
-
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator TestsWithEnumeratorPasses()
-        {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
-        }
-    }
+  [UnityTest]
+  public IEnumerator LaserDestroysAsteroid()
+  {
+    GameObject gameGameObject =
+    MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+    Game game = gameGameObject.GetComponent<Game>();
+    GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+    asteroid.transform.position = Vector3.zero;
+    GameObject laser = game.GetShip().SpawnLaser();
+    laser.transform.position = Vector3.zero;
+    yield return new WaitForSeconds(0.1f);
+    UnityEngine.Assertions.Assert.IsNull(asteroid);
+    Object.Destroy(game.gameObject);
+  }
 }
+
